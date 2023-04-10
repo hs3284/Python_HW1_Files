65f47f1ee3dc7904ef84a24ca2fa14ac61068f3e
hs3284 <105385743+hs3284@users.noreply.github.com>
2023-04-10 14:37:33 -0400

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,14 +1,13 @@
-
-
 def FizzBuzz(start, finish):
   outlist=[]
   for i in range (start,finish+1):
     if i % 3==0 and i % 5==0:
-      outlist.append('FizzBuzz')
+      outlist.append('fizzbuzz')
     elif i % 3==0:
-      outlist.append('Fizz')
+      outlist.append('fizz')
     elif i % 5==0:
-      outlist.append('Buzz')
+      outlist.append('buzz')
     else:
       outlist.append(i)
   return outlist
+
