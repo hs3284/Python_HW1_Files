290dd53925459d262fdfb129b5142263c969acde
hs3284 <105385743+hs3284@users.noreply.github.com>
2023-04-10 14:36:11 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,20 +1,10 @@
 
-def getBondPrice_E(face,couponRate,m,yc):
+def getBondPrice_E(face,couponRate,yc):
   coupon=face*couponRate
   pvcf=0
   for i,Y in enumerate(yc):
     pv = (1+Y)**(-(i+1))
     pvcf += coupon*pv
-  pvcftotal=face*(1+yc[-1])**(-m)+pvcf
+  pvcftotal=face*pv+pvcf
 
   return pvcftotal
-
-
-
-yc = [.010,.015,.020,.025,.030]
-face = 2000000
-couponRate = .04
-m = 5
-
-bondPrice = getBondPrice_E(face, couponRate, m, yc)
-print(bondPrice)
